3b2553951fad14df6b493cdf6da100386df9d0c4
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-12 00:30:16 +0330

    Add the functionality to courseSchema to calculate the average cost and call it before removing a course or after saving a course.

--- seeder.js
+++ seeder.js
@@ -42,7 +42,7 @@ const courses = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
+    // await Course.create(courses);
     console.log("✅ Data imported");
     process.exit();
   } catch (error) {
