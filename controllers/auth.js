c9170943b09614d0fc2a214d5ecb62ebbe47d112
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-28 17:38:08 +0330

    Create the sendEmail util which uses the nodemailer package and send the reset url to the client.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,10 @@
 const path = require("path");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middlewares/asyncHandler");
+const sendEmail = require("../utils/sendEmail");
 const geocoder = require("../utils/geocoder");
 // MODELS
 const User = require("../models/User");
-
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-  const token = user.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() +
-        process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === "production") {
-    options.secure = true;
-  }
-
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    token,
-  });
-};
 
 /*  ANCHOR
 @desc   Register User
@@ -65,17 +44,23 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   // Check for user
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select(
+    "+password"
+  );
 
   if (!user) {
-    return next(new ErrorResponse(`Invalid Credentials`, 401));
+    return next(
+      new ErrorResponse(`Invalid Credentials`, 401)
+    );
   }
 
   // check if password matches
   const isMatch = user.matchPassword(password);
 
   if (!isMatch) {
-    return next(new ErrorResponse(`Invalid Credentials`, 401));
+    return next(
+      new ErrorResponse(`Invalid Credentials`, 401)
+    );
   }
 
   sendTokenResponse(user, 200, res);
@@ -94,3 +79,85 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user, // I think sending req.user is enough, because this route is protected.
   });
 });
+
+/*  ANCHOR
+@desc   Forgot Password
+@route  GET /api/v1/auth/forgotpassword
+@access Public
+*/
+exports.forgotPassword = asyncHandler(
+  async (req, res, next) => {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (!user) {
+      return next(
+        new ErrorResponse(
+          `There's no user with that email`,
+          404
+        )
+      );
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create resetURL
+    const resetURL = `${req.protocol}://${req.get(
+      "host"
+    )}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `You're receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to :\n\n ${resetURL}`;
+
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: "Password Reset Token",
+        message,
+      });
+
+      res.status(200).json({
+        success: true,
+        data: "Email sent.",
+      });
+    } catch (error) {
+      console.error(`❌`, error);
+
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+      await user.save({ validateBeforeSave: false });
+
+      return next(
+        new ErrorResponse(`Email could not be sent`, 500)
+      );
+    }
+  }
+);
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() +
+        process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      token,
+    });
+};
